e3f091285f7a2ac596b4b7f4f19d50d1f78c15a5
StealthyPanda <shaikm259@gmail.com>
2025-02-18 23:34:57 +0530

    Added LoRA
    Minor changes in utils.

--- talos/utils.py
+++ talos/utils.py
@@ -25,11 +25,13 @@ def gpu_info() -> None:
     ndevices  = torch.cuda.device_count()
     print(f'Found {ndevices} cuda devices...')
     for each in range(ndevices):
+        intname = f'cuda:{each}'
         log(
-            each, '\t',
+            each, f' / {intname}', '\t',
             torch.cuda.get_device_name(each), '\t',
-            f'{torch.cuda.get_device_properties(0).total_memory / pow(2, 20):.2f}MB', '\t',
-            f'{torch.cuda.get_device_properties(0).total_memory / pow(2, 30):.2f}GB',
+            f'{torch.cuda.get_device_properties(each).total_memory / pow(2, 20):.2f}MB', '\t',
+            f'{torch.cuda.get_device_properties(each).total_memory / pow(2, 30):.2f}GB', #'\t',
+            # f'{torch.cuda.get_device_properties(each).name}',
         )
 
 
